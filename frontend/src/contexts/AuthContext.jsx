5ca91e218606adea855ec238ae817a48fc573238
Divyansh Patel <pateldivyansh131@gmail.com>
2025-06-10 23:15:34 +0530

    Final

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = import.meta.env.VITE_API_URL;
+axios.defaults.baseURL = "https://cyfuture-ai-srever.onrender.com";
 
 const AuthContext = createContext();
 //axios.defaults.baseURL = "http://localhost:5000";
